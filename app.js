52a43a865f9c5015bf1c54422f8ac6ff3ef6b344
Kieran Williams <kieranmarcus@me.com>
2020-10-08 03:40:31 +0100

    started using the youtube downloader features in the rest api

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ const session = require('express-session');
 
 
 
-const { from_txt_file } = require('./lib/importing-songs.js');
-const { get_video_data } = require('./lib/scrape-video-data.js');
-const { format_title } = require('./lib/format-title.js')
-const { get_search_urls } = require('./lib/convert-search-values-into-urls.js');
-const downloader = require('./lib/downloader');
+// const { from_txt_file } = require('./lib/importing-data.js/index.js');
+// const { get_video_data } = require('./lib/scrape-video-data.js');
+// const { format_title } = require('./lib/format-title.js')
+// const { get_search_urls } = require('./lib/convert-search-values-into-urls.js');
+// const downloader = require('./lib/downloader');
 
 
 const indexRouter = require('./routes/index');
@@ -53,6 +53,8 @@ app.use((req, res, next) => {
     res.locals.error = req.session.error || '';
     delete req.session.error;
 
+    req.session.progress = 0;
+
     // continue on to next function in middleware chain
     next();
 });
@@ -76,37 +78,6 @@ module.exports = app;
 
 
 
-// let songs = new Array;
-// let youtube_urls = new Array;
-
-
-// let video_search_values = from_txt_file('./file.txt');
-// let video_urls = get_search_urls(video_search_values);
-
-// console.log(video_urls);
-
-
-
-
-// (async () => {
-    
-//     let video_data = await get_video_data(video_urls);
-//     console.log(video_data);
-//     let url = video_data[0].url;
-//     let title = format_title(video_data[0].meta.title);
-//     downloader.download_audio(url, title);
-
-
-// })();
-
-
-
-
-// const port = process.env.PORT || 3000;
-// app.listen(port, (err) => {
-//     if (err) throw new Error(err);
-//     console.log(`Listening on http://localhost:${port}`);
-//   });
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs').promises;
 const { asyncErrorHandler } = require('../middleware/index.js');
+const { get_video_data } = require('../lib/yt-downloader.js');
+
 
 
 
@@ -15,7 +18,7 @@ router.get('/multi', (req, res, next) => {
 });
 
 /* POST search value */
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
     console.log(req.body.search);
     if (req.body.search.value === '') {
         req.session.error = "You must enter a Youtube video url or name"
@@ -23,7 +26,25 @@ router.post('/', (req, res, next) => {
         res.redirect('back');
     } 
     else {
+
+        // display loader until all data is back
         res.render('loader');
+
+        // turn search into text file 
+        const data = new Uint8Array(Buffer.from(req.body.search.value));
+        fs.writeFile('listings.txt', data, (err) => {
+        if (err) throw err;
+        console.log('The listings file has been saved!');
+        }).then(async () => {
+            // feed into get info on the listing(s)
+            let video_data = await get_video_data();
+            console.log('data: ', video_data);
+        });
+        
+        
+        
+
+        
     }
     
 });
